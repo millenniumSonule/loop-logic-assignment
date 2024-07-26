5991829491305c779189346ae8b0304dc6ad3f7e
millenniumSonule <anshumansonuley@gmail.com>
2024-07-26 20:54:52 +0530

    .

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+import './Homepage.css'
+
+const Homepage = () => {
+  return (
+    <div className="homepage-container">
+        <div className="nav-container">
+            <Navbar/>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Homepage
