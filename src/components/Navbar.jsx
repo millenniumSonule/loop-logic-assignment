240d144057aef0d46dc334650f0b7c10824ade4b
millenniumSonule <anshumansonuley@gmail.com>
2024-07-26 21:37:37 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <div className="navbar-logo">VIDEO</div>
+      <div className="navbar-links">
+        <a href="#video-games">VIDEO GAMES</a>
+        <a href="#contact">CONTACT</a>
+      </div>
 
-export default Navbar
+    </nav>
+  );
+};
+
+export default Navbar;
